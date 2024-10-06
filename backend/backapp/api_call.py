d8b118511559249f59e7bfc4449c13d9abe3bc6d
Shubham <gargshubham2005@gmail.com>
2024-10-06 01:59:45 -0400

    Fixed the keys

---
+++ backend/backapp/api_call.py
@@ -0,0 +1,36 @@
+import os
+from openai import AzureOpenAI
+
+
+AZURE_KEY = os.getenv('AZURE_KEY')
+
+def get_recipe(weight, ingredients):
+    client = AzureOpenAI(
+        api_key=AZURE_KEY,
+        api_version="2024-02-01",
+        azure_endpoint="https://wholeapp.openai.azure.com/"
+    )
+
+
+
+    # This will correspond to the custom name you chose for your deployment when you deployed a model.
+    # Use a gpt-35-turbo-instruct deployment.
+    deployment_name = "gpt-35-turbo-instruct"
+
+    # Send a completion call to generate an answer
+    prompt = ("Make an eco-friendly recipe using the ingredients from the list: " + ingredients + 
+    "with the most perishable ingredients being used first with the intent of " +
+    weight + "and keept it short and simple with nothing unnecesary")
+    response = client.completions.create(
+        model=deployment_name,
+        prompt=prompt,
+        temperature=1,
+        max_tokens=400,
+        top_p=0.6,
+        frequency_penalty=0,
+        presence_penalty=0,
+        best_of=1,
+        stop=None
+    )
+
+    print(response.choices[0].text)
--- backend/backapp/views.py
+++ backend/backapp/views.py
@@ -1,14 +1,42 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+import boto3
+import requests
+import os
+
+
+AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
+AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
+
+
 
 @api_view(['GET'])
 def upload(request):
-    query = request.GET.get('q', '')  # Get the search query parameter
-    results = proccess_upload(query)  # Implement your search logic here
+    weight = request.GET.get('w', '')  # Get the search query parameter
+    ingredient_s3 = request.GET.get('i', '')
+    results = proccess_upload(weight)  # Implement your search logic here
+    
     return Response({'results': results}, status=status.HTTP_200_OK)
+
+
+
+
 
 def proccess_upload(query):
     # Implement your search logic here
     # This is a dummy example returning static data
-    return [{"Hello": 123}]
+    dynamodb = boto3.resource('dynamodb',
+                          aws_access_key_id=AWS_ACCESS_KEY,
+                          aws_secret_access_key=AWS_SECRET_KEY,
+                          region_name="ca-central-1",
+                          endpoint_url="https://dynamodb.ca-central-1.amazonaws.com")
+    
+    table = dynamodb.Table("fridge-list")
+    item = {
+        "user": "gargshubham2005",
+        "food": []
+    }
+    response = table.put_item(Item=item)
+
+    return response
