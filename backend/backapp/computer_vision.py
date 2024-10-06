c155a28b65a6bce7d7d0ac00e7a93ff992d3187b
SHU8HAM1 <64797913+SHU8HAM1@users.noreply.github.com>
2024-10-06 09:40:20 -0400

    Update computer_vision.py

--- backend/backapp/computer_vision.py
+++ backend/backapp/computer_vision.py
@@ -4,13 +4,23 @@ import os
 # of the image we want as an input. Change these strings to run your own example.
 #################################################################################################
 
-CLARIFAI = os.getenv('CLARIFAIKEY')
+CLARIFAI = "key"
 
 PAT = CLARIFAI
 # Specify the correct user_id/app_id pairings
 # Since you're making inferences outside your app's scope
 USER_ID = 'clarifai'
 APP_ID = 'main'
+
+
+AWS_ACCESS_KEY = "key"
+AWS_SECRET_KEY = "key"
+####################################################################################
+# In this section, we set the user authentication, user and app ID, model details, and the URL
+# of the image we want as an input. Change these strings to run your own example.
+#################################################################################################
+
+import boto3
 
 
 # Change these to whatever model and image URL you want to use
@@ -25,7 +35,7 @@ from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
 from clarifai_grpc.grpc.api.status import status_code_pb2
 
 
-def get_ingredients(s3_key):
+def get_ingredients(username, s3_key):
     IMAGE_URL = f"https://fridge-food-images.s3.ca-central-1.amazonaws.com/{s3_key}"
 
     channel = ClarifaiChannel.get_grpc_channel()
@@ -59,6 +69,29 @@ def get_ingredients(s3_key):
     # Since we have one input, one output will exist here
     output = post_model_outputs_response.outputs[0]
 
+
+    dynamodb = boto3.resource('dynamodb',
+                          aws_access_key_id=AWS_ACCESS_KEY,
+                          aws_secret_access_key=AWS_SECRET_KEY,
+                          region_name="ca-central-1",
+                          endpoint_url="https://dynamodb.ca-central-1.amazonaws.com")
+    
+    table = dynamodb.Table("fridge-list")
+
+    food_list = []
+
     out = ""
     for concept in output.data.concepts:
+        food_list.append(concept.name)
         out += ("%s, " % (concept.name))
+    
+    response = table.update_item(
+            Key={'user':  username},
+            UpdateExpression='SET ingredients = :new_list',  # Specify the update expression
+            ExpressionAttributeValues={
+                ':new_list': food_list  # New value for the age attribute
+            },
+            ReturnValues='UPDATED_NEW'  # Return the new value of the updated attributes
+        )
+    
+    return out
