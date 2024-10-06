fe1c58ee7dc7d115d8f27e9ee2714db7259d890e
SHU8HAM1 <64797913+SHU8HAM1@users.noreply.github.com>
2024-10-06 09:40:45 -0400

    Update views.py

--- backend/backapp/views.py
+++ backend/backapp/views.py
@@ -4,6 +4,9 @@ from rest_framework import status
 import boto3
 import requests
 import os
+from .api_call import *
+from .computer_vision import *
+
 
 
 AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
@@ -13,17 +16,18 @@ AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
 
 @api_view(['GET'])
 def upload(request):
-    weight = request.GET.get('w', '')  # Get the search query parameter
-    ingredient_s3 = request.GET.get('i', '')
-    results = proccess_upload(weight)  # Implement your search logic here
+    profile = request.GET.get('q', '')  # Get the search query parameter
+    results = proccess_upload(profile)  # Implement your search logic here
     
     return Response({'results': results}, status=status.HTTP_200_OK)
 
 
 
+def proccess_upload(profile):
 
 
-def proccess_upload(query):
+    queries = profile.split("&")
+
     # Implement your search logic here
     # This is a dummy example returning static data
     dynamodb = boto3.resource('dynamodb',
@@ -34,9 +38,30 @@ def proccess_upload(query):
     
     table = dynamodb.Table("fridge-list")
     item = {
-        "user": "gargshubham2005",
-        "food": []
+        "user": queries[2],
+        "fname": queries[0],
+        "lname": queries[1],
+        "weight": queries[3],
+        "ingredients": []
     }
     response = table.put_item(Item=item)
 
-    return response
+    ingredients = get_ingredients(queries[2], queries[4])
+
+    answer = get_recipe(queries[3], ingredients)
+
+    result = answer.split("Ingredients")
+    temp = result[1].split("Instructions")
+    result[1] = temp[0]
+    result[2] = temp[1]
+    steps = []
+    j = 0
+    for i in result[2].strip():
+        if i in "1234567890":
+            steps.append("" + i)
+        steps[j] += i
+
+    return result
+
+queries = "shubham&garg&hello@gmail.com&max&fridge.jpg"
+print(proccess_upload(queries))
