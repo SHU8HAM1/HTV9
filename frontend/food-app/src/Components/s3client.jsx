e99a35d1b055b0d623bea064f02ff206d7ba3f88
devCodesApps <devoak11@gmail.com>
2024-10-06 01:52:57 -0400

    imp changes v2

--- frontend/food-app/src/Components/s3client.jsx
+++ frontend/food-app/src/Components/s3client.jsx
@@ -5,8 +5,8 @@ import { S3Client } from "@aws-sdk/client-s3";
 const s3Client = new S3Client({
   region: "your-region", // e.g., "us-east-1"
   credentials: {
-    accessKeyId: "", 
-    secretAccessKey: "",
+    accessKeyId: "",
+    secretAccessKey:"",
   },
 });
 
